9ba01891c103f53f1f861cf36d7382f01cdf95ff
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-04-04 02:27:16 +0300

    make it better code

--- index.php
+++ index.php
@@ -1,35 +1,42 @@
 <?php
-$sessionId= $_POST["sessionId"];
+// Read the variables sent via POST from our API
+$sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
-$text = $_POST["text"];
+$text        = $_POST["text"];
 
-if($text == ""){
-    $responce = "CON What would you want to check \n";
-    $responce = "1. My Account \n";
-    $responce = "1. My Phone Number \n";
-    // $responce = "1. My Account \n";
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
+
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
+    $response .= "2. Account balance";
+
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if($text == "1*1") { 
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
+
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
+
+} else if ( $text == "1*2" ) {
+    // This is a second level response where the user selected 1 in the first instance
+    $balance  = "KES 10,000";
+
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your balance is ".$balance;
 }
-else if($text == "1"){
-    $responce = "CON Choose Account information you want view \n";
-    $responce = "1. Account Number \n";
-    $responce = "1. Account BAlance \n";
-}else if($text == "2"){
-    $responce = "END Your Phone Number is".$phoneNumber;
 
-}else if($text == "1*1"){
-    $acctNumber = "ACC1001";
-
-    $responce = "END Your account Number is:".$acctNumber;
-
-}else if($text == "1*2"){
-    $balance = "KES 10,000";
-
-    $responce = "END Your Balance is:".$balance;
-
-}
-header('Content-type;text/plain');
-
-echo $responce;
-
-?>
+// Echo the response back to the API
+header('Content-type: text/plain');
+echo $response;
